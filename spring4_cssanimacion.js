97f55f212de9c5b459fbc98ba00522bff603c73f
carlosgp1700 <55363565+carlosgp1700@users.noreply.github.com>
2019-11-13 22:52:33 -0500

    Add files via upload

---
+++ spring4_cssanimacion.js
@@ -0,0 +1,6 @@
+var e = document.getElementById("animacion");
+e.addEventListener("animationstart", listener, false);
+e.addEventListener("animationend", listener, false);
+e.addEventListener("animationiteration", listener, false);
+
+e.className = "slidein";
